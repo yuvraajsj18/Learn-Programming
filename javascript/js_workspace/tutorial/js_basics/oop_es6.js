3c72767fc386cd4e2e666780ed900fd9a3ded082
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2020-06-16 18:56:23 +0530

    js basic done

--- javascript/js_workspace/tutorial/js_basics/oop_es6.js
+++ javascript/js_workspace/tutorial/js_basics/oop_es6.js
@@ -2,6 +2,7 @@
 
 class Person
 {
+    static a = 10;
     constructor(firstName, lastName, dob)
     {
         this.firstName = firstName;
@@ -20,3 +21,5 @@ const person2 = new Person('Mary', 'Smith', '12/02/1999');
 console.log(person2.getFullName());
 console.log(person2.getBirthYear());
 console.log(person2);
+
+console.log(Person.a);
