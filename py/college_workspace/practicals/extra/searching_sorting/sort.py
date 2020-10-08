a6cd650ff1c9cbabde07c93c8af1ad1d82ab80ae
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2020-10-08 21:50:25 +0530

    added test cases to sorting

--- py/college_workspace/practicals/extra/searching_sorting/sort.py
+++ py/college_workspace/practicals/extra/searching_sorting/sort.py
@@ -61,9 +61,45 @@ def main():
 
 if __name__ == "__main__":
     main()
+    unittest.main()
 
 
 # Test cases for all sorts
 
+class Tests(unittest.TestCase):
 
+    def test_bubble_sort(self):
+        lists = [
+            [5, 4, 3, 2, 1],
+            [3, 1, 4, 2, 5],
+            [1, 2, 3, 4, 5],
+            [3, 3, 3, 3, 3],
+            [1, 1, 1, 2, 2, 3, 3, 0, -5],
+        ]
 
+        for l in lists:
+            self.assertEqual(sorted(l), bubble_sort(list(l)))
+
+    def test_selection_sort(self):
+        lists = [
+            [5, 4, 3, 2, 1],
+            [3, 1, 4, 2, 5],
+            [1, 2, 3, 4, 5],
+            [3, 3, 3, 3, 3],
+            [1, 1, 1, 2, 2, 3, 3, 0, -5],
+        ]
+
+        for l in lists:
+            self.assertEqual(sorted(l), selection_sort(list(l)))
+
+    def test_insertion_sort(self):
+        lists = [
+            [5, 4, 3, 2, 1],
+            [3, 1, 4, 2, 5],
+            [1, 2, 3, 4, 5],
+            [3, 3, 3, 3, 3],
+            [1, 1, 1, 2, 2, 3, 3, 0, -5],
+        ]
+
+        for l in lists:
+            self.assertEqual(sorted(l), insertion_sort(list(l)))
