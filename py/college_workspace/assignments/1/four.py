1d2ea0441664b22c6026fd51a145caba88f147d0
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2020-10-09 21:14:25 +0530

    with for loops

--- py/college_workspace/assignments/1/four.py
+++ py/college_workspace/assignments/1/four.py
@@ -27,7 +27,7 @@ Samples:
 
 """
 
-def lists_to_dict(lists:list) -> dict:
+def lists_to_dict(lists:list) -> dict:  # SOLUTION
     """convert a list of lists into a dictionary by using
     values from first list as keys and list of corresponding values from other
     lists as value of that key.
@@ -52,19 +52,31 @@ def main():
     output_dict = lists_to_dict(input_list)
     assert output_dict == {4: [10, 19, 9], 5: [8, 4, 3], 7: [4, 6, 6]}
     print(output_dict)
+    print()
 
     input_list = [[4, 5, 7], [1, 2, 3], [1, 2]]
     output_dict = lists_to_dict(input_list)
     print(output_dict)
+    print()
 
     input_list = [[1, 2], [1, 2, 3], [4, 5, 6]]
     output_dict = lists_to_dict(input_list)
     print(output_dict)
+    print()
 
     input_list = [[4, 5, 7], [1, 2], [1, 2]]
     output_dict = lists_to_dict(input_list)
     print(output_dict)
 
-
 if __name__ == "__main__":
     main()
+
+"""Output:
+{4: [10, 19, 9], 5: [8, 4, 3], 7: [4, 6, 6]}
+
+{4: [1, 1], 5: [2, 2], 7: [3]}
+
+{1: [1, 4], 2: [2, 5]}
+
+{4: [1, 1], 5: [2, 2], 7: []}
+"""
