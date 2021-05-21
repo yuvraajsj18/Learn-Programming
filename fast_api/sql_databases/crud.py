7c5c4daba2db5daaeb4963f1d0d72b6d9830db60
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2021-05-21 23:13:37 +0530

    learning fastapi sql databases

--- fast_api/sql_databases/crud.py
+++ fast_api/sql_databases/crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 
-from . import models, schemas
+from sql_databases import models, schemas
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
